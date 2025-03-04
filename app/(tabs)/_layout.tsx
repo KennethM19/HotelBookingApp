a9e6c77a51e1c8a65391481ae752e90c7846cdbd
kenneth <126686075+KennethM19@users.noreply.github.com>
2025-03-04 12:50:58 -0500

    fix: icon navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,17 +20,17 @@ export default function TabLayout() {
             }} />
             <Tabs.Screen name='hotels' options={{
                 title: 'Hotels', tabBarIcon: ({ color, focused }) => (
-                    <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
-                )
-            }} />
-            <Tabs.Screen name='account' options={{
-                title: 'Account', tabBarIcon: ({ color, focused }) => (
-                    <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+                    <Ionicons name={focused ? 'business' : 'business-outline'} color={color} size={24} />
                 )
             }} />
             <Tabs.Screen name='favorites' options={{
                 title: 'Favorites', tabBarIcon: ({ color, focused }) => (
-                    <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+                    <Ionicons name={focused ? 'star' : 'star-outline'} color={color} size={24} />
+                )
+            }} />
+            <Tabs.Screen name='account' options={{
+                title: 'Account', tabBarIcon: ({ color, focused }) => (
+                    <Ionicons name={focused ? 'person' : 'person-outline'} color={color} size={24} />
                 )
             }} />
         </Tabs>
